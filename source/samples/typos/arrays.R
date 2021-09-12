94f7c9545a606bf81baab0fed071cd94899d531f
Rafael Gómez Bermejo <sernn2@gmail.com>
2021-09-12 19:35:58 +0200

    first commit - tipos de datos

---
+++ source/samples/typos/arrays.R
@@ -0,0 +1,7 @@
+# -------------
+# Ejemplos sobre las bases del array en R
+# ***
+# solo admiten un tipo de dato
+# -------------
+arr <- array(c(1:18), dim =c(3,6,1))
+print(arr)
---
+++ source/samples/typos/vectors.R
@@ -0,0 +1,12 @@
+# -------------
+# Ejemplos sobre las bases de los vectores en R
+# ***
+# solo admiten un tipo de dato
+# -------------
+ # Vector
+u1 <- c(1, 5, 4, 9, 0)
+print(u1)
+# Matrix
+# Data frame -> admits multiple modes
+# Ts
+# List  -> admits multiple modes
